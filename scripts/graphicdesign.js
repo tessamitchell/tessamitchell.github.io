ed29e334df7ebdbe8a3abb41e015351ab3987383
tessamit@icloud.com <tessamit@icloud.com>
2025-06-27 19:55:49 -0500

    new commit

--- scripts/graphicdesign.js
+++ scripts/graphicdesign.js
@@ -165,12 +165,12 @@ function expand(imgs,i) {
     // Get the expanded image
     var expandImg = allCards[i].querySelector("#expandedImg");
     // Get the image text
-    var imgText = allCards[i].querySelector("#imgdescription");
+    
     // Use the same src in the expanded image as the image being clicked on from the grid
     expandImg.src = imgs.src;
+    var imgText = allCards[i].querySelector("#imgdescription");
     // Use the value of the alt attribute of the clickable image as text inside the expanded image
     imgText.textContent = imgs.alt;
-    // Show the container element (hidden with CSS)
-    expandImg.parentElement.style.display = "block";
+
   }
 
